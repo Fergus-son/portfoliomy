a8febf85c4a4964d2f0c319bd97b65563083944e
Fergus-son <sarapovnikolaj790@gmail.com>
2025-02-01 19:26:14 +0300

    !

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-  base: '/c/coding/portfoliomy',
+  base: './coding/portfoliomy',
   plugins: [
     react({
       babel: {
