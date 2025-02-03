f73994405ad3da0078958ae58508ec51d1f39c87
Fergus-son <sarapovnikolaj790@gmail.com>
2025-02-03 07:38:30 +0300

    !

---
+++ src/components/icon/Icon.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import iconSprite from '../../assets/img/icon-sprites.svg'
+
+type IconPropsType = {
+    iconId: string
+    width?: string
+    height?: string
+    viewBox?: string
+}
+
+export const Icon = (props: IconPropsType) => {
+    return (
+        <svg width={props.width || "52"} height={props.height || "5"} viewBox={props.viewBox || "0 0 52 5"} fill="none" xmlns="http://www.w3.org/2000/svg">
+            <use xlinkHref={`${iconSprite}#${props.iconId}`}/>
+        </svg>
+    );
+};
---
+++ src/components/menu/Menu.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Menu = () => {
+    return (
+        <StyledMenu>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Skills</a></li>
+                    <li><a href="">Works</a></li>
+                    <li><a href="">Testimony</a></li>
+                    <li><a href="">Contacts</a></li>
+                </ul>
+            </nav>
+        </StyledMenu>
+    );
+};
+
+const StyledMenu = styled.nav `
+    ul{
+        display: flex;
+        gap: 30px;
+    }
+`
